05508b46f2b2ebccfb28f1621f129810950474de
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2024-03-08 11:31:35 -0300

    estrutura inicial do projeto

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+//const path = require('path')
+
+
+const routes = require('./routes/routes')
+
+const app = express()
+
+
+// habilita server para receber dados via post (formulário) 
+app.use(express.urlencoded({extended: true }))
+
+
+// definindo rotas
+app.use('/api', routes) // tudo que bater em api irá usar essas routes
+
+// executando servidor
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
