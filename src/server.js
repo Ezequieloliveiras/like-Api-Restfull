b5bac3def110c61f7880166c2c0ed402e62ed3bf
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2024-03-08 17:06:50 -0300

    adicionando middleware de cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-//const path = require('path')
+const cors = require('cors')
 
 const db = require('./database/db')
 const routes = require('./routes/routes')
@@ -7,6 +7,25 @@ const routes = require('./routes/routes')
 const app = express()
 
 db.connect()
+
+const allowedOrigins = [
+'http://localhost:3000'
+]
+
+
+
+// habilita cros
+app.use(cors({
+    origin: function(origin, callback) {
+        let allowed = true
+
+        if (!origin) allowed = true // aplicativo de celular
+
+        if(!allowedOrigins.includes(origin)) allowed = false
+
+        callback(null, allowed)
+    }
+}))
 
 // habilita server para receber dados json
 app.use(express.json())
