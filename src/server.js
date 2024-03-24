253dc43cd95e434a4b97107b2cf86c875779e603
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2024-03-23 21:14:48 -0300

    adicionando arquivos novos

--- src/server.js
+++ src/server.js
@@ -9,10 +9,10 @@ const app = express()
 db.connect()
 
 const allowedOrigins = [
-'http://localhost:3000'
+'http://localhost:3000',
+'http://localhost:3001',
+'http://127.0.0.1:5500'
 ]
-
-
 
 // habilita cros
 app.use(cors({
