d8606820a908027447ed67b9c086eee81f56156b
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2024-03-23 22:18:09 -0300

    adicionando url do render

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ const app = express()
 db.connect()
 
 const allowedOrigins = [
+'https://like-api-restfull.onrender.com',
 'http://localhost:3000',
 'http://localhost:3001',
 'http://127.0.0.1:5500'
